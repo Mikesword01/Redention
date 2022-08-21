1abe8b25a3a228cbdcb4167bdbac1eb584f7dced
Mikesword01 <miguelito010507@gmail.com>
2022-08-21 18:30:10 -0500

    Test
    Test

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,11 @@ public class CameraController : MonoBehaviour
 {
     Camera cam;
     
+    
     void Start()
     {
         cam = Camera.main;
+        Debug.Log("dev");
     }
 
     // Update is called once per frame
@@ -17,4 +19,5 @@ public class CameraController : MonoBehaviour
         Vector3 pos = new Vector3(this.transform.position.x,this.transform.position.y,-10);
         cam.transform.position = pos;
     }
+    
 }
